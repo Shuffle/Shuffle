b6e14f91fe7105e251cbf653048b9990947b53cc
frikky <fredrik_9490@hotmail.com>
2020-05-23 10:59:51 +0200

    Ready for new update

--- frontend/run.sh
+++ frontend/run.sh
@@ -11,7 +11,7 @@ echo "Starting server"
 # Rerun build locally for it to update :)
 docker run -it \
 	-p 3001:80 \
-	-p 3443:443 \
+	-p 3002:443 \
 	-v $(pwd)/build:/usr/share/nginx/html:ro \
 	--rm \
 	nginx	
