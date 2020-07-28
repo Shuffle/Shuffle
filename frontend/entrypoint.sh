825da6c131cfef0ba8cd0041ab4dfe2a4981e2f4
Harduino <mail@harduino.com>
2020-07-28 11:46:07 +0300

    siemonster :: frontend - wait for backend readiness

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# prepare variables
+BACKEND_HOSTNAME=${BACKEND_HOSTNAME:-shuffle-backend}
+
+# wait for backend readiness
+while [ "$(curl -s http://$BACKEND_HOSTNAME:5001/api/v1/_ah/health)" != "OK" ]; do
+    echo "Waiting for backend readiness..."
+    sleep 5
+done
+
 # generate configs
 /usr/local/bin/confd -backend="env" -confdir="/etc/confd" -onetime
 
