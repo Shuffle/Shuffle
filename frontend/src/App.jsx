66513a177dc9960d8eabbbce8a0d66d872428b7d
amitk <amitk@infopercept.com>
2021-03-15 14:43:55 +0530

    Myview dashboard - new page, If we want to replace it with a workfolow page need to currect route file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,8 @@ import LandingPageNew from "./views/LandingpageNew";
 import LoginPage from "./views/LoginPage";
 import SettingsPage from "./views/SettingsPage";
 
+import MyView from "./views/MyView";
+
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
 import AlertTemplate from "./components/AlertTemplate";
@@ -148,6 +150,7 @@ const App = (message, props) => {
 			<Route exact path="/docs" render={props => { window.location.pathname = "/docs/about" }} />
 			<Route exact path="/introduction" render={props => <Introduction isLoaded={isLoaded} globalUrl={globalUrl} {...props} />} />
 			<Route exact path="/introduction/:key" render={props => <Introduction isLoaded={isLoaded} globalUrl={globalUrl} {...props} />} />
+			<Route exact path="/myview" render={props => <MyView cookies={cookies} removeCookie={removeCookie} isLoaded={isLoaded} isLoggedIn={isLoggedIn} globalUrl={globalUrl} cookies={cookies} userdata={userdata} {...props} />} />
 			<Route exact path="/" render={props => <LoginPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} register={true} isLoaded={isLoaded} globalUrl={globalUrl} setCookie={setCookie} cookies={cookies} {...props} />} />
 		</div>
 
