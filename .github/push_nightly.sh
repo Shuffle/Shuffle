3d8665439a8855dc3a88693e8a7a4e6ef57d66ea
frikky <fredrik_9490@hotmail.com>
2022-07-25 12:54:44 +0200

    Added nightly push script to run post workflow manually

---
+++ .github/push_nightly.sh
@@ -0,0 +1,5 @@
+# This can be done in the dockerpush workflow itself
+docker pull frikky/shuffle-backend:nightly
+docker pull frikky/shuffle-frontend:nightly
+docker tag frikky/shuffle-backend:nightly ghcr.io/frikky/shuffle-backend:nightly
+docker tag frikky/shuffle-frontend:nightly ghcr.io/frikky/shuffle-frontend:nightly
