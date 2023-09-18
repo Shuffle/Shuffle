f1171ee2226686644d5035672e8cb11845afadbb
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-09-18 22:59:52 +0530

    fix: subloop read-only typeerror bug

--- frontend/src/views/AppCreator.jsx
+++ frontend/src/views/AppCreator.jsx
@@ -286,7 +286,7 @@ const getJsonObject = (properties) => {
 	for (let key in properties) {
 		const property = properties[key];
 
-		const subloop = false
+		let subloop = false
 		if (property.hasOwnProperty("type")) {
 			if (property.type === "object" || property.type === "array") {
 				subloop = true
