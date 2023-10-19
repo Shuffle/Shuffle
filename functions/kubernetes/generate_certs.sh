aad43c913d07ca0772ffe111d93c27a192935581
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-10-19 17:06:23 +0530

    fix: making the script functional

--- functions/kubernetes/generate_certs.sh
+++ functions/kubernetes/generate_certs.sh
@@ -12,7 +12,17 @@ fi
 
 echo "Using node IP: $node_ip to generate SSL certs!"
 
+mkdir -p certs
+
 # Generate CA key
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/reg.key -x509 -days 365 -out certs/reg.crt -subj "/CN=$node_ip"
 
+# generate a random string
+random_string=$(openssl rand -hex 3)
+
+docker run -d -p 5000:5000 --restart=always --name "shuffle-local-registry-$random_string" \
+  -v $(pwd)/certs:/certs \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/reg.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/reg.key \
+  registry:2
 
