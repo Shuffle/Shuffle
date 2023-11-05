9c1f0419362e7e8cad5e4e94b9c3890db8043d37
Aditya Saini <adityasaini10012001@outlook.com>
2023-11-05 11:44:55 +0530

    Fix import statement for 'App' component in index.js
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 
 //import "./index.css";
 //import reportWebVitals from "./reportWebVitals";
