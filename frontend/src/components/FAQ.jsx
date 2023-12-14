dfa6167de9e71ae48c5a2c7a78614d690bdd197c
Monil Prajapati <monillprajapatii@gmail.com>
2023-12-15 00:22:58 +0530

    Revert "Added FAQ component"
    This reverts commit 0bd16551008491f90f2a91919e546b192fecc192.

--- frontend/src/components/FAQ.jsx
+++ frontend/src/components/FAQ.jsx
@@ -1,31 +1,22 @@
-import React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React, {useState} from 'react';
+
 
 const FAQItem = (props) => {
 	const { question, answer } = props
 
-	return (
-		<Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography variant='body1'>
-			{question}
-		  </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography variant='body2'>
-           {answer}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+	const [isExpanded, setIsExpanded] = useState(false)
 
+	return (
+		<Paper onClick={() => {
+			setIsExpanded(!isExpanded)	
+		}}>
+			<Typography variant="body1">
+				{question}
+			</Typography>
+			<Typography variant="body2" color="textSecondary">
+				{answer}
+			</Typography>
+		</Paper>
 	)
 }
 
