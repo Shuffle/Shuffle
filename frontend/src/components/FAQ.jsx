28224bb88296b9ef8ee88341e151556459c47049
Monil Prajapati <monillprajapatii@gmail.com>
2024-01-04 22:01:50 +0530

    Updated FAQ Component

--- frontend/src/components/FAQ.jsx
+++ frontend/src/components/FAQ.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
+const classes = {
+  box: {
+    fontSize: 60,
+    color:"white",
+    opacity: 0.9,
+    backgroundColor: "#27292d",
+  },
+};
 
-const FAQItem = (props) => {
-  const { question, answer } = props
+const FAQItem = () => {
+  // const { question, answer } = props
 
   return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
+    <div>
+      <Accordion
+      sx={classes.box}
       >
-        <Typography variant='body1'>
-          {question}
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography variant='body2'>
-          {answer}
-        </Typography>
-      </AccordionDetails>
-    </Accordion>
-  )
-}
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon sx={{color:"white"}} />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="body1">
+            {/* {question} */}
+            How can i switch to annual billing?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="body2">
+            {/* {answer} */}
+            Contact us at{" "}
+            <span className="underline text-blue-500">Contact</span> Page
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
 
 export default FAQItem;
