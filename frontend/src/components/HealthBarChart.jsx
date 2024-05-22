2de1f1355a621a282dccd16a13f6e891f9a5280d
Aditya <60684641+0x0elliot@users.noreply.github.com>
2024-05-22 16:34:25 +0530

    feat[health-page]: adding health page to onprem

---
+++ frontend/src/components/HealthBarChart.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const HealthBarChart = (props) => {
+    const { globalUrl, filteredData, options, onBarClick  } = props;
+
+    return (
+        <Bar
+            data={filteredData}
+            options={options}
+            height="35.5rem"
+            width={filteredData.width}
+            getElementAtEvent={(elements) => {
+                if (elements && elements.length > 0) {
+                    onBarClick(elements);
+                }
+            }}
+        />
+    );
+};
+
+export default HealthBarChart;
