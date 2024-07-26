56d96b331a69fac6890959086110542cbe5077c6
Frikky <frikky@shuffler.io>
2024-07-26 17:02:29 +0200

    Force build new nginx without confd

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# generate configs
-/usr/local/bin/confd -backend="env" -confdir="/etc/confd" -onetime
+# generate configs - is this necessary? 
+# Removing confd if possible
+#/usr/local/bin/confd -backend="env" -confdir="/etc/confd" -onetime
 
 # run main command
 exec "$@"
